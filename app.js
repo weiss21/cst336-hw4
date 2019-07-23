6915328a5802e8a1486c5dae2d21ddc8b848625c
waisalirobleh <waisalirobleh@gmail.com>
2019-07-23 07:51:53 -0400

    positioning

---
+++ app.js
@@ -0,0 +1,43 @@
+/* Code Below mostly borrowed from Lab 4
+Faker API learned from source: https://github.com/marak/Faker.js/
+Express API learned from CSt 336 Summer 2019 course.
+*/
+
+const express = require("express"); //imports Express Library
+const app = express(); //variable "app" to access methods.
+var faker = require('faker'); //imports faker
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+var port = process.env.PORT || "8000"; //To work in both local (codeanywhere) and heroku
+var serial = process.env.IP || "0.0.0.0"; // same as comment above
+//server listener original example
+//app.listen("8081", "0.0.0.0", function(){
+//  console.log("Express Server is Running...")
+//});
+
+
+// Heroku update server listener
+app.listen(port, serial, function() {
+  console.log("Express Server is Running. . . ")
+});
+
+
+app.get("/", function(req, res) {
+  res.render("index.html");
+});
+
+app.get("/ios", function(req, res) {
+  res.render("ios.html");
+});
+
+app.get("/android", function(req, res) {
+  res.render("android.html");
+});
+
+app.get("/other", function(req, res) {
+  res.render("other.html");
+});
+
+
+
